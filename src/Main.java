8208241fba3f57cf78edfac9c2c3a953d63f5902
Puskar Uprety <upretypuskar@gmail.com>
2022-12-19 15:27:37 -0500

    The basics of solar system are ready and deploye

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        SolarSystem xx = new SolarSystem();
+        xx.sun=1;
+        xx.moon=2;
+        xx.planets=3;
+
+    }
+}
